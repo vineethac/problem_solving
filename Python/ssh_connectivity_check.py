efa76bab3f9fa3adf3a2b7f3f7050a6d403cfd79
vineethac <vineethac88@gmail.com>
2024-05-30 18:54:21 +0530

    added ssh_connectivity_check.py

---
+++ Python/ssh_connectivity_check.py
@@ -0,0 +1,36 @@
+'''
+Given a list of IP or hostnames of machines.
+Write a script that will check whether ssh connection 
+to it is successful or not.
+
+'''
+import getpass
+import paramiko
+
+def verify_ssh_connetion(hosts, username, password):
+    """
+    Function that checks ssh connection.
+    """
+    for host in hosts:
+        client = paramiko.client.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            client.connect(host, username=username, password=password)
+            print(f"SSH connection to host {host} successful.")
+            client.close()
+        except:
+            print(f"SSH connection to host {host} failed.")
+
+def main():
+    """
+    Main function.
+    """
+    hosts = ["h1", "h2", "h3"]
+    username = input("Enter username: ")
+    password = getpass.getpass(prompt="Enter password: ")
+
+    verify_ssh_connetion(hosts, username, password)
+
+
+if __name__ == "__main__":
+    main()
